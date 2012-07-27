d8bf138c79cf8d6401d1501e2562e1e8724f4302
mezun7 <shuhrat94@ya.ru>
2012-07-28 00:52:26 +0400

    TEST

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//email:shuhrat94@ya.ru
+//skype: shuhrat1994
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string sk, em;
+	em = "email: shuhrat94@ya.ru";
+	sk = "skype: shuhrat1994";
+	cout << em << endl << sk;
+	return 0;
+}
